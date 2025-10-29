ae01581eae992717a61285d47cb88aabd6d0f06d
JuanArenas08 <jparenas@espol.edu.ec>
2025-10-29 14:15:17 -0500

    commite 3
    Push

--- taller/src/main/java/com/espol/PersonalAcademico.java
+++ taller/src/main/java/com/espol/PersonalAcademico.java
@@ -3,16 +3,24 @@ package com.espol;
 public class PersonalAcademico extends Usuario {
     private String email;
     private String contenido;
-    public PersonalAcademico (String usuario, String contrasena, String nombre, String apellido, String email, String contenido){
+    private String comentario;
+    public PersonalAcademico (String usuario, String contrasena, String nombre, String apellido, String email, String contenido, String comentario){
             super(usuario, contrasena, nombre, apellido);
             this.email = email;
             this.contenido = contenido;
+            this.comentario = comentario;
         }
     public String getEmail(String email){
         return this.email;
     }
     public String getCotenido(String contenido){
         return this.contenido;
+    }
+    public String getComentario(String comentario){
+        return this.comentario;
+    }
+    public void setComentario(String comentario){
+        this.comentario = comentario;
     }
 
     public void setEmail(String email){
@@ -25,6 +33,9 @@ public class PersonalAcademico extends Usuario {
         //Algo
     }
     public void solicitarAsesoramientoDelSistema(String email, String contenido){
+        //Algo
+    }
+    public void comentarEnForo(Foro foro, String comentario){
 
     }
 }
